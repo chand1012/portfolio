e5d74e3c7b629222339fdd8b2f4f9aa717503a5a
chand1012 <chandler@chand1012.dev>
2022-01-20 15:25:05 -0500

    Add apple and android icons to code

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,6 +41,23 @@ const Home: NextPage = () => {
           content="A collection of my projects and works."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
 
       <NavBar />
